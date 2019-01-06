5dde35b5491217fcc1973c557369e85acf5b87c1
b.palowski <brianpalowski00@gmail.com>
2019-01-05 18:37:46 -0800

    created views files

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+
 
 const app = express();
 
-//router
+//ejs
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+//routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
