03d730c49834304d777b540bcbaca9e0da292eb9
b.palowski <brianpalowski00@gmail.com>
2019-01-05 18:33:41 -0800

    set up route index and users

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const router = express.Router();
+//login
+router.get('/login', (req,res) => res.send("login"));
+//register
+router.get('/register', (req,res) => res.send("Register"));
+module.exports = router;
